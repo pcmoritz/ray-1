057a32df48ba0c096761b28dd1e66a692533284b
Philipp Moritz <pcmoritz@gmail.com>
2017-06-09 16:44:30 -0700

    initial spectral hyperoptimization code

---
+++ examples/hyperopt/hyperopt_spectral.py
@@ -0,0 +1,71 @@
+from collections import OrderedDict
+import itertools
+from sklearn.linear_model import lasso_path
+import numpy as np
+import random
+
+d = 2
+
+config = OrderedDict([("optimizer", ("SGD", "Adam")),
+                      ("learning_rate", (0.01, 0.003, 0.001, 0.0003, 0.0001, 0.00003)),
+                      ("layer1_num_filters", (16, 32, 64)),
+                      ("layer1_num_filters", (16, 32, 64)),
+                      ("layer3_num_filters", (512, 1024, 2048)),
+                      ("layer1_stddev", (0.001, 0.003, 0.01, 0.03)),
+                      ("layer2_stddev", (0.001, 0.003, 0.01, 0.03)),
+                      ("layer3_stddev", (0.001, 0.003, 0.01, 0.03)),
+                      ("layer4_stddev", (0.001, 0.003, 0.01, 0.03))])
+
+def sample_params(config):
+  return OrderedDict([(key, random.choice(val)) for key, val in config.items()])
+
+hyperparameters = {"optimizer": "Adam",
+                   "learning_rate": 1e-3,
+                   "layer1_num_filters": 32,
+                   "layer2_num_filters": 64,
+                   "layer3_num_filters": 1024,
+                   "layer1_stddev": 0.01,
+                   "layer2_stddev": 0.01,
+                   "layer3_stddev": 0.01,
+                   "layer4_stddev": 0.01}
+
+
+def polynomial_sparse_recovery(X, b, d=2, plot=False):
+  T = X.shape[0] # number of random experiments
+  n = X.shape[1] # number of hyperparameters
+  indices = []
+  for k in range(1, d+1):
+    indices.extend(itertools.combinations_with_replacement(range(n), k))
+  A = np.zeros((T, len(indices)))
+  for i in range(T):
+    for j in range(len(indices)):
+      A[i, j] = np.product(X[i, indices[j]])
+  alphas_lasso, coeffs_lasso, _ = lasso_path(A, b, eps=1e-3)
+  if plot:
+    import matplotlib.pyplot as plt
+    colors = itertools.cycle(['b', 'r', 'g', 'c', 'k'])
+    neg_log_alphas_lasso = -np.log10(alphas_lasso)
+    for coeff, c in zip(coeffs_lasso, colors):
+      l1 = plt.plot(neg_log_alphas_lasso, coeff, c=c)
+    plt.xlabel('-Log(alpha)')
+    plt.ylabel('coefficients')
+    plt.title('Lasso and Elastic-Net Paths')
+    plt.axis('tight')
+  import IPython
+  IPython.embed()
+
+from tensorflow.examples.tutorials.mnist import input_data
+import mnist
+
+if __name__ == "__main__":
+  # Load the mnist data and turn the data into remote objects.
+  print("Downloading the MNIST dataset. This may take a minute.")
+  dataset = input_data.read_data_sets("MNIST_data", one_hot=True)
+  train_images = ray.put(dataset.train.images.reshape(-1, 28, 28, 1))
+  train_labels = ray.put(dataset.train.labels)
+  validation_images = ray.put(dataset.validation.images.reshape(-1, 28, 28, 1))
+  validation_labels = ray.put(dataset.validation.labels)
+
+  accuracy_id = mnist.train_cnn_and_compute_accuracy.remote(
+        hyperparameters, 1000, train_images, train_labels, validation_images,
+        validation_labels)
---
+++ examples/hyperopt/mnist.py
@@ -0,0 +1,93 @@
+# Most of the tensorflow code is adapted from Tensorflow's tutorial on using
+# CNNs to train MNIST
+# https://www.tensorflow.org/versions/r0.9/tutorials/mnist/pros/index.html#build-a-multilayer-convolutional-network.  # noqa: E501
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import ray
+import tensorflow as tf
+
+slim = tf.contrib.slim
+
+def get_batch(data, batch_index, batch_size):
+  # This method currently drops data when num_data is not divisible by
+  # batch_size.
+  num_data = data.shape[0]
+  num_batches = num_data // batch_size
+  batch_index %= num_batches
+  return data[(batch_index * batch_size):((batch_index + 1) * batch_size)]
+
+
+def cnn_setup(params, images, labels, num_classes, is_training=True, scope='LeNet'):
+  with tf.variable_scope(scope, 'LeNet', [images, num_classes]):
+    net = slim.conv2d(images, params["layer1_num_filters"], [5, 5],
+                      weights_initializer=tf.truncated_normal_initializer(stddev=params["layer1_stddev"]),
+                      scope='conv1')
+    net = slim.max_pool2d(net, [2, 2], 2, scope='pool1')
+    net = slim.conv2d(net, params["layer2_num_filters"], [5, 5],
+                      weights_initializer=tf.truncated_normal_initializer(stddev=params["layer2_stddev"]),
+                      scope='conv2')
+    net = slim.max_pool2d(net, [2, 2], 2, scope='pool2')
+    net = slim.flatten(net)
+
+    net = slim.fully_connected(net, params["layer3_num_filters"],
+                               weights_initializer=tf.truncated_normal_initializer(stddev=params["layer3_stddev"]),
+                               scope='fc3')
+    net = slim.dropout(net, 0.5, is_training=True, scope='dropout3')
+    logits = slim.fully_connected(net, num_classes, activation_fn=None,
+                                  weights_initializer=tf.truncated_normal_initializer(stddev=params["layer4_stddev"]),
+                                  scope='fc4')
+  output = tf.nn.softmax(logits)
+  cross_entropy = tf.reduce_mean(-tf.reduce_sum(labels * tf.log(output),
+                                 reduction_indices=[1]))
+  correct_pred = tf.equal(tf.argmax(output, 1), tf.argmax(labels, 1))
+  if params["optimizer"] == "Adam":
+    optimizer = tf.train.AdamOptimizer(params["learning_rate"])
+  elif params["optimizer"] == "SGD":
+    optimizer = tf.train.GradientDescentOptimizer(params["learning_rate"])
+  else:
+    assert False, "Unknown optimizer {}".format(params["optimizer"])
+  return (optimizer.minimize(cross_entropy),
+          tf.reduce_mean(tf.cast(correct_pred, tf.float32)), cross_entropy)
+
+
+# Define a remote function that takes a set of hyperparameters as well as the
+# data, consructs and trains a network, and returns the validation accuracy.
+@ray.remote
+def train_cnn_and_compute_accuracy(params, steps, train_images, train_labels,
+                                   validation_images, validation_labels,
+                                   weights=None):
+  # Extract the hyperparameters from the params dictionary.
+  batch_size = 128
+  # Create the network and related variables.
+  with tf.Graph().as_default():
+    # Create the input placeholders for the network.
+    images = tf.placeholder(tf.float32, shape=[None, 28, 28, 1])
+    labels = tf.placeholder(tf.float32, shape=[None, 10])
+    # Create the network.
+    train_step, accuracy, loss = cnn_setup(params, images, labels, 10)
+    # Do the training and evaluation.
+    with tf.Session() as sess:
+      # Use the TensorFlowVariables utility. This is only necessary if we want
+      # to set and get the weights.
+      variables = ray.experimental.TensorFlowVariables(loss, sess)
+      # Initialize the network weights.
+      sess.run(tf.global_variables_initializer())
+      # If some network weights were passed in, set those.
+      if weights is not None:
+        variables.set_weights(weights)
+      # Do some steps of training.
+      for i in range(1, steps + 1):
+        # Fetch the next batch of data.
+        image_batch = get_batch(train_images, i, batch_size)
+        label_batch = get_batch(train_labels, i, batch_size)
+        # Do one step of training.
+        sess.run(train_step, feed_dict={images: image_batch, labels: label_batch})
+      # Training is done, so compute the validation accuracy and the current
+      # weights and return.
+      totalacc = accuracy.eval(feed_dict={images: validation_images,
+                                          labels: validation_labels})
+      new_weights = variables.get_weights()
+  return float(totalacc), new_weights
