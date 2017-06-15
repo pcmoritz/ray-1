64a05df9528ca32253ad748aaf4731fe2a96b52b
Philipp Moritz <pcmoritz@gmail.com>
2017-06-14 18:02:16 -0700

    more tests

--- multiple_local_schedulers.sh
+++ multiple_local_schedulers.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 for i in {1..100}
 do
-   python test/runtest.py ResourcesTest .testMultipleLocalSchedulers
+  python test/runtest.py ResourcesTest.testMultipleLocalSchedulers
 done
 
+for i in {1..100}
+do
+  python test/component_failures_test.py ComponentFailureTest.testPlasmaStoreFailed
+done
