02dbbdda4a5a795e9cc26ce52db98bd82b9c786a
Philipp Moritz <pcmoritz@gmail.com>
2018-02-07 19:55:06 -0800

    update

--- examples/sonic/train_ppo.py
+++ examples/sonic/train_ppo.py
@@ -61,6 +61,7 @@ config.update({
   "clip_param": 0.2,
   "num_sgd_iter": 20,
   "sgd_batchsize": 4096,
+  "use_gae": False,
   "devices": ["/gpu:0"],
   "tf_session_args": {
     "gpu_options": {"allow_growth": True}
@@ -69,6 +70,6 @@ config.update({
 
 alg = ppo.PPOAgent(config=config, env=env_name, registry=get_registry())
 
-for i in range(10):
+for i in range(100):
     result = alg.train()
     print("result = {}".format(result))
