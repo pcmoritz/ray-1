ae1b9a18d5008a8c1663c85e6d892f4cd07e81ab
Philipp Moritz <pcmoritz@gmail.com>
2018-03-30 16:31:19 -0700

    temp commit

--- src/ray/raylet/node_manager.cc
+++ src/ray/raylet/node_manager.cc
@@ -222,7 +222,13 @@ void NodeManager::ProcessClientMessage(std::shared_ptr<LocalClientConnection> cl
     RAY_LOG(DEBUG) << "reconstructing object " << object_id.hex();
     RAY_CHECK_OK(object_manager_.Pull(object_id));
   } break;
-
+  case protocol::MessageType_PlasmaFetchRequest: {
+    auto message = flatbuffers::GetRoot<protocol::PlasmaFetchRequest>(message_data);
+    // TODO(pcm): Batch these messages
+    for (auto &object_id : from_flatbuf(*message->object_ids())) {
+      RAY_CHECK_OK(object_manager_.Pull(object_id));
+    }
+  } break;
   default:
     RAY_LOG(FATAL) << "Received unexpected message type " << message_type;
   }
