7bb2e14b52a0cfe4e84dec712fc7f5717fe7c181
Philipp Moritz <pcmoritz@gmail.com>
2019-03-29 15:42:33 -0700

    update

--- src/ray/raylet/raylet_client.cc
+++ src/ray/raylet/raylet_client.cc
@@ -246,7 +246,7 @@ ray::Status RayletClient::SubmitTaskFast(const std::vector<ObjectID> &execution_
   fbb.Finish(message);
   auto payload = std::string(fbb.GetBufferPointer(), fbb.GetBufferLength());
   auto s = db_->Put(rocksdb::WriteOptions(), task_spec.TaskId().binary(), payload);
-  RAY_CHEK(s.ok());
+  RAY_CHECK(s.ok());
   ray::Status::OK();
 }
 
