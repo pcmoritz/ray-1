11eb5e0ecccef64222d3669b2bf75c0b1becb1ec
Philipp Moritz <pcmoritz@gmail.com>
2021-08-06 23:41:43 -0700

    make worker_id 28 bytes

--- src/ray/gcs/gcs_server/gcs_actor_scheduler.cc
+++ src/ray/gcs/gcs_server/gcs_actor_scheduler.cc
@@ -96,7 +96,7 @@ void GcsActorScheduler::Schedule(std::shared_ptr<GcsActor> actor) {
         rpc::Address address;
         address.set_ip_address("test-actor.default.svc.cluster.local");
         address.set_port(7891);
-        address.set_worker_id("test-actor");
+        address.set_worker_id("test-actor000000000000000000");
         auto client = core_worker_clients_.GetOrConnect(address);
         std::cout << "connected" << std::endl;
       }
